25edcdb2973ac2e0dd466fa9ff3efa928cc4af3b
devtushar20 <devtushar20@gmail.com>
2026-06-05 20:57:33 +0530

    If-else,if-else ladder,nested if-else

---
+++ src/basics/ConditionalStatement2.java
@@ -0,0 +1,40 @@
+//Switch statement
+package basics;
+
+import java.util.Scanner;
+
+public class ConditionalStatement2 {
+    static void main(String[] args) {
+        //basic switch case example
+
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int day = sc.nextInt();
+        switch (day) {
+            case 1:
+                System.out.println("Today is sunday");
+                break;
+            case 2:
+                System.out.println("Today is monday");
+                break;
+            case 3:
+                System.out.println("Today is tuesday");
+                break;
+            case 4:
+                System.out.println("Today is wednesday");
+                break;
+            case 5:
+                System.out.println("Today is thursday");
+                break;
+            case 6:
+                System.out.println("Today is friday");
+                break;
+            case 7:
+                System.out.println("Today is saturday");
+                break;
+            default:
+                System.out.println("Invalid day");
+        }
+
+    }
+}
