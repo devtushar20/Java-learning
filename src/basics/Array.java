096118b197b3f2c59b9ecddefb6ccb8225f02f24
devtushar20 <devtushar20@gmail.com>
2026-06-08 11:51:03 +0530

    Day 05 - Added Arrays practice

---
+++ src/basics/Array.java
@@ -0,0 +1,48 @@
+package basics;
+
+public class Array {
+    static void main(String[] args) {
+//        int age[] = new  int[5];
+//        age[0]=5;
+//        age[1]=6;
+//
+//        System.out.println(age[0]);
+//        System.out.println(age[1]);
+//        System.out.println(age[2]);
+//
+//        System.out.println(age.length);//length is used to find the size of the array
+
+//        int marks[]={90,95,98,93,94};//you can also initialize array like this
+//        System.out.println(marks[3]);
+
+//
+      // int numbers[]={12,23,34,45,56}; //sum using for loop
+//        int sum=0;
+//        for(int number :numbers){
+//            sum+=number;
+//
+//        }
+//        System.out.println("The sum is: "+sum);
+
+//        int max;
+//        max = Integer.MIN_VALUE;
+//        for(int number :numbers){
+//            if(number >max){
+//                max= number;
+//            }
+//        }
+//        System.out.println("The maximum value is "+max);
+
+// 2-D  Array marks of student
+        int marks[][]={
+                {12,20,32},
+                {20,22,23},
+                {33,44,55}
+        };
+        System.out.println(marks[1][1]);
+
+
+
+
+    }
+}
